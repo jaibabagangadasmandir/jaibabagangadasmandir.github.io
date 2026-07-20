e1f36ebce2d7b9a8a5c7d41d966a57bd84776a06
jaibabagangadasmandir <sandeepsnp.haryana@gmail.com>
2026-07-20 10:53:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -32,3 +32,41 @@ lightbox.addEventListener("click", () => {
     lightbox.classList.remove("active");
 
 });
+// Mobile Menu
+
+const menu = document.getElementById("menu-toggle");
+const nav = document.getElementById("nav-links");
+
+menu.addEventListener("click", () => {
+    nav.classList.toggle("active");
+});
+
+// Back To Top
+
+const topBtn = document.getElementById("topBtn");
+
+window.onscroll = function () {
+
+    if(document.documentElement.scrollTop > 300){
+
+        topBtn.style.display="block";
+
+    }else{
+
+        topBtn.style.display="none";
+
+    }
+
+};
+
+topBtn.onclick = function(){
+
+    window.scrollTo({
+
+        top:0,
+
+        behavior:"smooth"
+
+    });
+
+};
